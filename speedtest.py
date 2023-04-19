2cf3c378242bcbb62b384b2808a38707b455f800
Idk <35537849+Dio-Off-Brando@users.noreply.github.com>
2023-04-19 12:29:55 +0200

    cleaning repo

--- speedtest.py
+++
@@ -1,8 +0,0 @@
-import torrentAggregator
-import time
-
-startTime = time.time()
-
-for i in torrentAggregator.search("demon slayer" ):
-    print(str(i) + '\n\n')
-print(f"execution time was {time.time()-startTime})")
